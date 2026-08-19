907292088249f51595757c6b5f5c32a44542289b
bindagearyan-bit <bindagearyan@gmail.com>
2026-08-19 14:33:35 +0530

    Update Find the Index of the First Occurrence.py

--- Find the Index of the First Occurrence.py
+++ Find the Index of the First Occurrence.py
@@ -1,7 +1,3 @@
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
-        if needle not in haystack:
-            return -1
-
-        if needle in haystack:
-            return haystack.index(needle)   
+        return -1 if needle not in haystack else haystack.index(needle) 
